66dd0fd3d06b47831d9b6ce93ddf428b5dc0a4b1
KayleeMcLaren <134863501+KayleeMcLaren@users.noreply.github.com>
2023-09-20 14:04:19 +0200

    Update index.js
    Added more comments

--- 14.Secrets/index.js
+++ 14.Secrets/index.js
@@ -4,14 +4,19 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Creates an Express application and assigns it to the app variable
+// Also sets the port number that the application will listen on
 const app = express();
 const port = 3000;
 
+// Declares a global variable called userIsAuthorised and sets it to false 
+// Will be used to track whether the user is logged in
 var userIsAuthorised = false;
 
+// Tells Express to use the bodyParser middleware to parse incoming requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// checks if the user enters the correct password
+// Checks if the user enters the correct password
 function passwordCheck(req, res, next) {
   const password = req.body["password"];
   if (password === "ILoveProgramming") {
@@ -19,22 +24,26 @@ function passwordCheck(req, res, next) {
   }
   next();
 }
-app.use(passwordCheck);
+// This function is registered as middleware using the app.use() method 
+// This means that it will be called for every incoming request
+app.use(passwordCheck); 
 
+// This route handler handles GET requests to the root URL (/). It sends the index.html file to the client
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "index.html");
 });
 
+// Checks if the user is logged in (i.e., if the userIsAuthorised variable is set to true)
 app.post("/check", (req, res) => {
-  // if the user logs in with the correct password
+  // If the user logs in with the correct password
   if (userIsAuthorised) {
-    res.sendFile(__dirname + "secret.html"); // the secrets are dispalyed
+    res.sendFile(__dirname + "secret.html"); // The secrets are dispalyed
   } else {
-    res.sendFile(__dirname + "index.html"); 
-    //Alternatively res.redirect("/");
+    res.sendFile(__dirname + "index.html"); // Otherwise, the index.html file is sent to the client
   }
 });
 
+// Starts the Express application on the specified port
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
