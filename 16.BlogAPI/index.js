c44cd90f274787937fe9a567f55c14b378bb28cb
KayleeMcLaren <134863501+KayleeMcLaren@users.noreply.github.com>
2023-10-05 21:34:28 +0200

    Add files via upload
    This code creates a simple Express API for managing posts. The API has four endpoints:
    
    posts: GET all posts, POST a new post
    posts/:id: GET a specific post by ID, PATCH a specific post by ID, DELETE a specific post by ID
    
    The API uses an in-memory data store to store the posts. This is not ideal for production use, but it is sufficient for a simple example.

---
+++ 16.BlogAPI/index.js
@@ -0,0 +1,107 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 4000;
+
+// In-memory data store
+let posts = [
+  {
+    id: 1,
+    title: "The Rise of Decentralized Finance",
+    content:
+      "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+    author: "Alex Thompson",
+    date: "2023-08-01T10:00:00Z",
+  },
+  {
+    id: 2,
+    title: "The Impact of Artificial Intelligence on Modern Businesses",
+    content:
+      "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+    author: "Mia Williams",
+    date: "2023-08-05T14:30:00Z",
+  },
+  {
+    id: 3,
+    title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
+    content:
+      "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
+    author: "Samuel Green",
+    date: "2023-08-10T09:15:00Z",
+  },
+];
+
+let lastId = 3;
+
+// Middleware
+// Registers the Body Parser middleware with the Express app. 
+// This middleware will be used to parse the request bodies of all incoming requests.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// GET all posts
+// Returns the in-memory data store of posts
+app.get("/posts", (req, res) => {
+  console.log(posts);
+  res.json(posts);
+});
+
+// GET a specific post by id
+// Finds the corresponding post in the in-memory data store and returns it, or returns a 404 error if the post cannot be found
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  res.json(post);
+});
+
+// POST a new post
+// Creates a new post object using the data in the request body, adds it to the in-memory data store, and returns the new post with a 201 Created status code
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
+
+// PATCH a post when you just want to update one parameter
+// Finds the corresponding post in the in-memory data store and updates any of its properties that are present in the request body. 
+// It then returns the updated post
+app.patch("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+
+  res.json(post);
+});
+
+// DELETE a specific post by providing the post id
+// Finds the corresponding post in the in-memory data store and removes it. 
+app.delete("/posts/:id", (req, res) => {
+  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
+  posts.splice(index, 1);
+  res.json({ message: "Post deleted" });
+});
+
+// Starts the Express app on the specified port
+app.listen(port, () => {
+  console.log(`API is running at http://localhost:${port}`);
+});
+
+// This code creates a simple Express API for managing posts. The API has four endpoints:
+
+//posts: GET all posts, POST a new post
+//posts/:id: GET a specific post by ID, PATCH a specific post by ID, DELETE a specific post by ID
+//The API uses an in-memory data store to store the posts. This is not ideal for production use, but it is sufficient for a simple example.
